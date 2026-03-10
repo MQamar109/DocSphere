b542d91e97448c737b3020c9540a1d9fe0713d14
MQamar109 <qamar.mushtaq@cogentlabs.co>
2026-03-10 15:23:54 +0500

    update migrations file

--- DocSphere/settings.py
+++ DocSphere/settings.py
@@ -34,10 +34,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'organization',
     'user',
     'workspace',
-    'core',
 ]
 
 MIDDLEWARE = [
