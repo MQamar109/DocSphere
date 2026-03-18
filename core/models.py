a1984bb54958116f73172518877df4dea5d75c19
MQamar109 <qamar.mushtaq@cogentlabs.co>
2026-03-18 12:01:20 +0500

    enh: improve code according to the PEP8

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
-from django_extensions.db.models import TimeStampedModel
 from django.db import models
+
+from django_extensions.db.models import TimeStampedModel
+
 
 class BaseModel(TimeStampedModel):
     is_active = models.BooleanField(default=True)
-    
+
     class Meta:
         abstract = True
