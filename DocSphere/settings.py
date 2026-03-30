290b774053f0fc9d67bc78f262aac0fe4e3979c5
MQamar109 <qamar.mushtaq@cogentlabs.co>
2026-03-30 20:14:56 +0500

    added login screen ui

--- DocSphere/settings.py
+++ DocSphere/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/api/v1/user/list/'
+LOGIN_REDIRECT_URL = '/user/list/'
 LOGOUT_REDIRECT_URL = '/core/login/'
 
 LOGIN_URL = '/core/login/'
